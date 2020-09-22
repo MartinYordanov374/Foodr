3a0273483723e5f2696fc1596ae21ca430814ede
GoofyGalaxy374 <30842987+GoofyGalaxy374@users.noreply.github.com>
2020-09-23 00:08:43 +0300

    -You can now remove individual items from the cart

--- src/Components/cart.js
+++ src/Components/cart.js
@@ -1,111 +1,125 @@
-import React, { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React, {useState, useRef, useEffect} from 'react';
 import {Navbar, Nav, Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import {LinkContainer} from 'react-router-bootstrap'
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
-import {MdLineWeight, MdClear, MdHome, MdInfo, MdPhone,MdCheck} from 'react-icons/md'
-import './cart.css'
+import {MdShoppingCart,MdClear, MdLineWeight, MdPhone, MdInfo} from 'react-icons/md'
 import $ from 'jquery'
-import item from './Index'
-function Cart() {
-    const[products, setProducts] = useState([]);
-    let x = JSON.parse(localStorage.getItem('products'))
-    let counter = x.length
-    let buttonProductsCounter = x.length
+import './Index.css'
 
-    const showSideNav =()=>
+import Burger from './Images/Burger.png'
+import Pizza from './Images/Pizza.png'
+
+function Index()
+{
+    
+    const[product, setProduct] = useState([])
+    const[item] = useState([{
+        name: 'Burger',
+        image: '/static/media/Burger.bcd6f0a3.png',
+        id: 0,
+        price: 16.00
+    },
     {
-        $('.sideNavMenu').css('width', '250px')
+        name: 'Pizza',
+        image: '/static/media/Pizza.07b5b3c1.png',
+        id: 1,
+        price: 20.00
+    }])
+
+    const showSideMenu=()=>
+    {
+        $('.sideMenu').css('width', '250px')
         $('.mainContent').css('marginLeft', '250px')
-        $('.mainContent').css('opacity', '0.6')
+
+        $('body').css('backgroundColor', ' rgba(170, 0, 255, 0.6)')
+
+    }
+
+    const hideSideMenu=()=>
+    {
+        $('.sideMenu').css('width', '0px')
+        $('.sideMenu').css('marginLeft', '0px')
+        $('.mainContent').css('marginLeft', '0px')
         
 
+        $('body').css('backgroundColor', 'rgba(170, 0, 255, 1)')
+
     }
-    const hideSideNav =()=>
+    const addToCart=(item)=>
     {
-        $('.sideNavMenu').css('width', '0px')
-        $('.mainContent').css('marginLeft', '0px')
-        $('.mainContent').css('opacity', '1')
-
-
+        setProduct([...product, item])
+        localStorage.setItem('products', JSON.stringify(product))
+        
     }
-    const getItems=()=>{
-            counter-=1
-            if(counter<=x.length)
-            {
-                setProducts([...products, x])
-
-            }
-
+    const removeItem=(idx)=>
+    {
+        let x = product.filter(item=>item.id != idx)
+        setProduct([product, x])
+        $('.removeButton').remove()
     }
 
-
-    return (
-        <div >
-            <div className='sideNavMenu'>
-                <span onClick={hideSideNav} className='hideSideNavButton'>
+    return(
+        <div>
+             <div className='sideMenu'>
+                <span className='hideSideBar'onClick={hideSideMenu}>
                     <MdClear></MdClear>
                 </span>
-                <h3 className='sideNavHeading'>How can we help you?</h3>
-
-                <LinkContainer className='sideNavLink' to='/home'>
-                    <Nav.Link>
-                        <MdHome></MdHome> Home
-                    </Nav.Link>
-                </LinkContainer>
-                <br></br>
-                <br></br>
-                <LinkContainer className='sideNavLink' to='/home'>
-                    <Nav.Link>
-                        <MdInfo></MdInfo> About us
-                    </Nav.Link>
+                <h3 className='sideMenuTitle'>How can we help you?</h3>
+                <LinkContainer to='/about'>
+                    <Nav.Link className='sideLink'><MdInfo></MdInfo> About us</Nav.Link>
                 </LinkContainer>
                 <br></br>
                 <br></br>
 
-                <LinkContainer className='sideNavLink' to='/home'>
-                    <Nav.Link>
-                        <MdPhone></MdPhone> Contact us
-                    </Nav.Link>
+                <LinkContainer to='/'>
+                    <Nav.Link className='sideLink'><MdPhone></MdPhone> Contact Us</Nav.Link>
                 </LinkContainer>
-            </div>
-            <div className='mainContent'>
-
-                <Navbar variant='dark'>
-                    <Navbar.Brand>Your Cart</Navbar.Brand>
-                    <Nav className='ml-auto'>
-                        <span className='showSideNavButton' onClick={showSideNav}>
-                            <MdLineWeight></MdLineWeight>
-                        </span>
-                    </Nav>
-                </Navbar>
-                <div className='products'>   
-                        <div>
-                            <Button onClick={getItems}> Show items in the cart ( {buttonProductsCounter} left to show )</Button>
-                            {products.map((product, index)=>(
-                                <div className='product'key={index}>
-                                    <h2 className='productName'>{product[index].name}</h2>
-                                    <img className = 'productImage' src={product[index].image}></img>
-                                    <h2>${product[index].price}.00</h2>
-                                </div>
-                            ))}
-                        </div>
+                <div>
+                    <div>
                     
-                     <Button variant='success' id='removeProductButton' className='removeProductButton' >
-                         <span>
-                             <strong>
-                                    <MdCheck>
-                                    </MdCheck>
-                                        Finish your order
-                             </strong>
-                           </span>
-                    </Button>
+                    {product.map((item, idx)=>
+                    <div className='yourCart' key={idx}>
+                        <hr></hr>
+                        <div className={idx}>
+                            <img src ={item.image}></img>
+                            <h3 className='burgerTitle'>{item.name}</h3>
+                            <h4><strong>$ {item.price}.00</strong></h4>
+                            <Button variant='warning' className='removeButton' onClick={()=>removeItem(idx)}>Remove</Button>
+                        </div>
+                        <br></br>
+                  </div>)}
+                    </div>
                 </div>
             </div>
+            <div className='mainContent'>
+                <Navbar variant='dark' bg=''>
+                    <Navbar.Brand>Home</Navbar.Brand>
+                        
+                        <Nav className='ml-auto'>
+                            <LinkContainer to='/cart' className='goToCartPage'>
+                                <Nav.Link>
+                                    <MdShoppingCart></MdShoppingCart>
+                                </Nav.Link>
+                            </LinkContainer>
+                            <span className='showSideBar' onClick={showSideMenu}>
+                                <MdLineWeight></MdLineWeight>
+                            </span>       
+                        </Nav>
+                </Navbar>
+                <div className='foodList'>
+                  {item.map((item, idx)=>
+                  <div className={item.name} key={idx}>
+                        <img src ={item.image}></img>
+                        <h3 className='burgerTitle'>{item.name}</h3>
+                        <h4><strong>$ {item.price}.00</strong></h4>
+                        <Button variant='warning' className ='purchaseButton' onClick={()=>addToCart(item)} key={idx} ><strong><MdShoppingCart></MdShoppingCart>Add to cart</strong></Button>
+                        <br></br>
+                  </div>)}
+                </div>
+               
+            </div>
         </div>
-    );
-
+        
+    )
 }
-
-
-export default Cart;
+export default Index;
